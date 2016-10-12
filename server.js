3a2cd46f00703ddce11ccb70cd254bb344f757ae
Jesus D <jesus.diaz@cide.edu>
2016-10-11 19:40:55 -0500

    Initalize repo

---
+++ server.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var PORT = 3000;
+
+var middleware = {
+	requireAthentication: function (req, res, next) {
+		console.log('Private');
+		next();
+	},
+	logger: function (req, res, next) {
+		console.log( new Date().toString() + req.method + ' ' + req.originalUrl );
+		next();
+	}
+} 
+
+app.use(middleware.logger);
+
+app.use(express.static(__dirname + '/public'));
+
+app.get('/about', function (req, res) {
+	res.send('Hola about');
+})
+
+
+
+app.listen(PORT, function() {
+	console.log('El servidor esta corriendo, puerto ' + PORT);
+});
+
